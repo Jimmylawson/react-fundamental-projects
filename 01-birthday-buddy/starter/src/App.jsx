eb0678c9214b95d07890d59b7b5f0c4b55e21c07
jimmy <jimoseipeace2bb@gmail.com>
2025-03-27 19:12:23 -0500

    Finish working on the components and the css. Close #2

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,42 @@
+import { useState } from "react";
+import data from "./data";
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  const [people, setPeople] = useState(data);
+  const [show, setShow] = useState(false);
+  const clearAll = () => {
+    setPeople([]);
+    setShow(true);
+  };
+
+  const showPeople = () => {
+    setPeople(data);
+    setShow(false);
+  };
+
+  return (
+    <div>
+      <h1>{people.length} Birthdays Today</h1>
+      <div className="container">
+        {people.map((person) => {
+          let { id, name, age, image } = person;
+          return (
+            <div className="card" key={id}>
+              <img src={image} alt="person picture" className="img" />
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          );
+        })}
+      </div>
+      <div className="button-group">
+        <button className="clear" onClick={clearAll}>
+          Clear All
+        </button>
+        <button className="clear" onClick={showPeople}>
+          Show
+        </button>
+      </div>
+    </div>
+  );
 };
 export default App;
